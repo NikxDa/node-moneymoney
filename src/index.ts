f512241689d7635463a91bd3a4646fbae8f7a4e6
NikxDa <NikxDa@users.noreply.github.com>
2023-12-27 12:34:53 +0100

    Remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import { addTransaction } from "./transactions.js";
-
 export * from "./accounts.js";
 export * from "./categories.js";
 export * from "./transactions.js";
 export { checkDatabaseUnlocked } from "./utils.js";
-
-addTransaction({
-    account: "offline account",
-    amount: 20,
-    date: new Date(),
-    to: "Test payee",
-});
