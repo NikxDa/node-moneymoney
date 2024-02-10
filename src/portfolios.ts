5e6e6b5c0452775af02369d689e32384442ebd0f
NikxDa <NikxDa@users.noreply.github.com>
2024-02-10 01:29:42 +0100

    Add getPortfolio

---
+++ src/portfolios.ts
@@ -0,0 +1,38 @@
+import { tellMoneyMoney } from "./utils.js";
+
+type AccountBalance = [number, string];
+
+export enum AccountType {
+    GIRO = "Giro account",
+    SAVINGS = "Savings account",
+    FIXED_TERM_DEPOSIT = "Fixed term deposit",
+    LOAN = "Loan account",
+    CREDIT_CARD = "Credit card",
+    OTHER = "Other",
+    GROUP = "Account group",
+}
+
+export type Account = {
+    accountNumber: string;
+    // attributes: any; TODO: Figure out what this is
+    balance: AccountBalance[];
+    bankCode: string;
+    currency: string;
+    group: boolean;
+    icon: Buffer;
+    indentation: number;
+    name: string;
+    owner: string;
+    portfolio: boolean;
+    type: AccountType;
+    uuid: string;
+};
+
+type GetPortfolioOptions = {
+    account?: string;
+    assetClass?: string;
+};
+
+export const getPortfolio = (options: GetPortfolioOptions) => {
+    return tellMoneyMoney<Account[]>("export accounts");
+};
